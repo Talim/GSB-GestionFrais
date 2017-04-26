a5a3a86ceae604514049ae17a4d78cbf5cf34c00
Dylkiller <dylan_83000@hotmail.fr>
2017-04-26 16:02:30 +0200

    Update color compta

--- Ressources/GSB_Appli/vues/v_accueil.php
+++ Ressources/GSB_Appli/vues/v_accueil.php
@@ -13,20 +13,28 @@ else {
 </div>
 <div class="row">
     <div class="col-md-12">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
+      <?php
+        if ($compta){?>
+        <div class="panel panel-primary color-compta-border">
+            <div class="panel-heading color-compta-back color-compta-border">
                 <h3 class="panel-title">
                     <span class="glyphicon glyphicon-bookmark"></span> Navigation</h3>
             </div>
             <div class="panel-body">
                 <div class="row">
                     <div class="col-xs-12 col-md-12 text-center">
-                      <?php
-                      if ($compta){?>
                         <a href="index.php?uc=validerFrais&action=selectionnerVisiteur" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-pencil"></span> <br/>Valider fiche de frais</a>
-                        <a href="index.php?uc=suivreFrais&action=selectionnerFiche" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Suivre paiement fiches de frais</a>
-                <?php }
-                      else {?>
+                        <a href="index.php?uc=suivreFrais&action=selectionnerFiche" class="btn btn-primary btn-lg color-compta-back color-compta-border" role="button"><span class="glyphicon glyphicon-list-alt "></span> <br/>Suivre paiement fiches de frais</a>
+          <?php }
+            else {?>
+            <div class="panel panel-primary">
+                <div class="panel-heading ">
+                      <h3 class="panel-title">
+                          <span class="glyphicon glyphicon-bookmark"></span> Navigation</h3>
+                </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12 text-center">
                         <a href="index.php?uc=gererFrais&action=saisirFrais" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-pencil"></span> <br/>Renseigner la fiche de frais</a>
                         <a href="index.php?uc=etatFrais&action=selectionnerMois" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Afficher mes fiches de frais</a>
                 <?php }?>
